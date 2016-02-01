bfe5379f95aef6c1548ea05cf1eff9897743901e
Jonah Haney <jdh104@users.noreply.github.com>
2016-02-01 17:46:49 -0600

    Complete Problem004
    Obscure numeric solution

--- Problem004.java
+++ Problem004.java
@@ -6,7 +6,7 @@ The largest palindrome made from the product of two 2-digit numbers is 9009 = 91
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 
-Solution: 906609
+Solution: XYZZYX
 */
 
 public class Problem004{
