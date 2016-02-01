4fc9d52a3a98f84b6eedd88bb6ddd1ed9bbd02ae
Jonah Haney <jdh104@users.noreply.github.com>
2016-02-01 00:26:21 -0600

    Update MathUtil.java
    Added:
    * prime checking
    * factorials

--- MathUtil.java
+++ MathUtil.java
@@ -1,8 +1,20 @@
 
 
 public class MathUtil{
-
-    public static int factorial(int operand){
     
+    public static boolean isPrimeNumber(long operand){
+        long limit = ((long) Math.floor(Math.sqrt((double) operand)));
+        for (long i=2; i<=limit; i++){
+            if (operand % i == 0){
+                return false;
+            }
+        } return true;
+    }
+    
+    public static long factorial(int operand){
+        long answer = 1;
+        for (int i=1; i<operand; i++){
+            answer *= i;
+        } return answer;
     }
 }
