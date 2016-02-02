b91e494890e74269142c2824c78036425099bebd
Jonah Haney <jdh104@users.noreply.github.com>
2016-02-01 18:41:13 -0600

    Complete Problem009

--- Problem009.java
+++ Problem009.java
@@ -1,1 +1,38 @@
+/*
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+Solution: ########
+*/
+
 public class Problem009{
+    
+    private static final long SUM = 1000;  //Given in problem
+    
+    public static void main(String[] args){
+        System.out.println(getAnswer());
+    }
+    
+    /**
+     * Chose to use a method so that I could use
+     * return to exit the loop early.
+     * @return the answer to Problem 9, negative one (-1) if there is no valid answer.
+     */
+    public static long getAnswer(){
+        for (long c=0; c<SUM; c++){
+            for (long b=0; b<c; b++){
+                for (long a=0; a<b; a++){
+                    if ((Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2)) && (a + b + c == SUM)){
+                        return a * b * c;
+                    }
+                }
+            }
+        } return (-1L);
+    }
+}
