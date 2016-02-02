47b501a314a8328ac8191a3b783211846598af84
Jonah Haney <jdh104@users.noreply.github.com>
2016-02-01 18:45:06 -0600

    Complete Problem010
    This one was strikingly easy compared to the last few.

--- Problem010.java
+++ Problem010.java
@@ -1,1 +1,22 @@
+/*
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+Solution: ############
+*/
+
 public class Problem010{
+
+    private static final long MAX = 2000000;
+    
+    public static void main(String[] args){
+        long sum = 2L;
+        for (long i=3L; i<MAX; i+=2){
+            if (MathUtil.isPrimeNumber(i)){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
